92ca127ba08553f47a3a71ab5f008cd88ac88dfa
Icy Cloud <i2cy@outlook.com>
2022-05-14 01:16:46 +0800

    updated api docs for functions in class FileSession, added DOWN command

--- i2ftpserver/server.py
+++ i2ftpserver/server.py
@@ -27,41 +27,80 @@ FILE_SESSION_TIMEOUT = 120
 FULL_SPEED_TIMEOUT = 20
 
 
+__VERSION = "1.0"
+
+
 class FileSession:
 
-    def __init__(self, path):
-        self.io = open(path, "rb+")
+    def __init__(self, path, readonly=True):
+        """
+        file I/O session object
+
+        :param path: Path like object
+        :param readonly: bool, set session to readonly
+        """
+        if readonly:
+            self.io = open(path, "rb")
+        else:
+            self.io = open(path, "rb+")
+        self.readonly = readonly
         self.sha256 = hashlib.sha256()
         self.size = os.path.getsize(path)
-        self.__flag_sha256_avaliable = False
-        self.__fp = 0
+        self.__flag_sha256_available = False
+        self.fp = 0
         self.__sha256_fp = 0
         self.__lock_io = False
         self.__last_io_ts = time.time()
 
     def seek(self, offset):
+        """
+        change current file pointer to given offset
+
+        :param offset: int
+        :return: None
+        """
+        if self.fp == offset:
+            return
+
         if self.__lock_io:
             time.sleep(0.001)
         self.__lock_io = True
         self.__last_io_ts = time.time()
 
-        self.__fp = offset
+        self.fp = offset
         self.io.seek(offset)
 
         self.__lock_io = False
 
     def write(self, data):
+        """
+        write data to standard file I/O
+
+        :param data: bytes
+        :return: int, length
+        """
         if self.__lock_io:
             time.sleep(0.001)
         self.__lock_io = True
         self.__last_io_ts = time.time()
 
-        ret = self.io.write(data)
+        try:
+            ret = self.io.write(data)
+        except Exception as err:
+            ret = 0
+
+        self.fp += ret
 
         self.__lock_io = False
         return ret
 
     def read(self, length):
+        """
+        read data from standard file I/O
+
+        :param length: int, max data length
+        :return: bytes, data
+        """
         if self.__lock_io:
             time.sleep(0.001)
         self.__lock_io = True
@@ -69,11 +108,20 @@ class FileSession:
 
         ret = self.io.read(length)
 
+        self.fp += len(ret)
+
         self.__lock_io = False
         return ret
 
     def calc_sha256(self, step=True, size=8192):
-        if self.__flag_sha256_avaliable:
+        """
+        calculate file's sha256 sum value by step or instantly
+
+        :param step: bool, calculate one step
+        :param size: int, read size for one step
+        :return: bool, calculation status (False for already done the calculation)
+        """
+        if self.__flag_sha256_available:
             return False
         if self.__lock_io:
             time.sleep(0.001)
@@ -84,7 +132,7 @@ class FileSession:
             data = self.io.read(size)
             length = len(data)
             if length < size:
-                self.__flag_sha256_avaliable = True
+                self.__flag_sha256_available = True
             self.__sha256_fp += length
             self.sha256.update(data)
         else:
@@ -93,25 +141,41 @@ class FileSession:
                 if not data:
                     break
                 self.sha256.update(data)
-            self.__flag_sha256_avaliable = True
+            self.__flag_sha256_available = True
 
-        self.io.seek(self.__fp)
+        self.io.seek(self.fp)
 
         self.__lock_io = False
 
         return True
 
     def age(self):
+        """
+        return the time (s) of this session from the last interact
+
+        :return: float, time in seconds
+        """
         return time.time() - self.__last_io_ts
 
     def sha256(self):
+        """
+        return file's sha256 sum, if calculation is still in progress,
+        status will be False
+
+        :return: (bool status, hex_str sum)
+        """
         self.__last_io_ts = time.time()
-        if self.__flag_sha256_avaliable:
+        if self.__flag_sha256_available:
             return True, self.sha256.hexdigest()
         else:
             return False, None
 
     def close(self):
+        """
+        close session
+
+        :return: None
+        """
         self.io.close()
 
 
@@ -119,21 +183,21 @@ class I2ftpServer:
 
     def __init__(self, config):
         """
-        I2FTP对象
+        I2FTP object
 
         :param config: i2ftpserver.Config
         """
 
         assert isinstance(config, Config)
 
-        self.__header = "[i2ftp]"
+        self.__header = "[I2FTP]"
         self.config = config
         self.__server = None
         self.logger = Logger(config.log_file,
                              level=config.log_level)
         self.root = pathlib.Path(config.ftp_root)
         if not self.root.is_absolute():
-            self.logger.CRITICAL("{} [init] root path has to be absolute path")
+            self.logger.CRITICAL("{} [Init] root path has to be absolute path")
             raise Exception("root path in configuration has to be absolute path")
         if not self.root.exists():
             path_fixer(config.ftp_root)
@@ -144,11 +208,7 @@ class I2ftpServer:
 
         self.connections = []  # handler池
         self.__cmd_queue = queue.Queue(maxsize=MAX_CMD_QUEUE)  # 命令池
-        # 文件会话对象储存格式：{<会话ID>:[最后活动的时间戳,
-        #                             文件路径,
-        #                             文件sha256对象,
-        #                             文件大小,
-        #                             最后一次sha256计算文件指针偏移量]}
+
         self.__file_session = {}  # 文件会话池
 
     def start(self):
@@ -257,15 +317,34 @@ class I2ftpServer:
             if not path.exists() or path.is_dir():
                 return b"\x00,path does not exist or target is a directory"
 
-            session = FileSession(path)
+            # 建立会话
+            session = FileSession(path, readonly=True)
 
-            session_id = random_keygen(64)
+            session_id = random_keygen(16)
             while session_id in self.__file_session:
-                session_id = random_keygen(64)
+                session_id = random_keygen(16)
 
             self.__file_session.update({session_id: session})
 
             ret = b"\x01," + session_id
+
+        elif cmd == b"DOWN":  # 通过会话下载文件
+            # 分割指令
+            session_id = payload[:16]
+            fp = int().from_bytes(payload[17:], "little", signed=True)
+
+            # 检查会话是否有效
+            if session_id not in self.__file_session:
+                return b"\x00,invalid session id quested"
+
+            session = self.__file_session[session_id]
+            assert isinstance(session, FileSession)
+
+            # 下发数据
+            session.seek(fp)
+            data = session.read(8192)
+            ret = b"\x01," + data
+
 
         return ret
 
@@ -287,6 +366,7 @@ class I2ftpServer:
                 # 自动关闭不活动的会话
                 if session.age() > FILE_SESSION_TIMEOUT:
                     session.close()
+                    self.__file_session.pop(ele)
                     continue
 
                 # 计算会话文件的sha256校验和
@@ -294,7 +374,7 @@ class I2ftpServer:
                 if ret:
                     full_speed_ts = time.time()
 
-                if full_speed_ts > FULL_SPEED_TIMEOUT:
+                if time.time() - full_speed_ts > FULL_SPEED_TIMEOUT:
                     time.sleep(0.01)
 
     def __loop(self):
